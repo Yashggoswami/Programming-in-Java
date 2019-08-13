f429efa8075272048cfa80fcca27ce5b231ef12b
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-08-13 20:32:35 +0530

    Q8

---
+++ src/Q8.java
@@ -0,0 +1,14 @@
+class   Hello{
+    void output(){
+        System.out.println("java welcomes you");
+    }
+
+}
+
+public class Q8 {
+    public static void main(String[] args) {
+        Hello ob=new Hello();
+        ob.output();
+    }
+
+}

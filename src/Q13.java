cc3428ab787b6856a905b003b80ef2351a302ef8
Yashggoswami <41734202+Yashggoswami@users.noreply.github.com>
2019-08-16 22:51:01 +0530

    Q13

---
+++ src/Q13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Q13 {
+    public static void main(String[] arg)
+    {
+        {
+            int[] marks = { 125, 132, 95, 116, 110 };
+
+            int highest_marks = maximum(marks);
+            System.out.println("The highest score is " + highest_marks);
+        }
+    }
+    static int maximum(int[] numbers)
+    {
+        int maxSoFar = numbers[0];
+
+
+        for (int num : numbers)
+        {
+            if (num > maxSoFar)
+            {
+                maxSoFar = num;
+            }
+        }
+        return maxSoFar;
+    }
+}
